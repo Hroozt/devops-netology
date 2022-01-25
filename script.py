2a3654ba5266df7f1c290c7aeeb6a4ee5c2a50ad
Aitov <hroozt@mail.ru>
2022-01-25 18:30:20 +0700

    Edit Readme.MD

---
+++ script.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import os
+
+bash_command = ["cd ~/netology/sysadm-homeworks", "git status"]
+result_os = os.popen(' && '.join(bash_command)).read()
+is_change = False
+for result in result_os.split('\n'):
+    if result.find('modified') != -1:
+        prepare_result = result.replace('\tmodified:   ', '')
+        print(prepare_result)
+        break
