f2c8b2f0fd39710c779262904bca191f09bfd098
Aitov <hroozt@mail.ru>
2022-07-07 20:04:45 +0700

    homework 73

--- keys.sh
+++ keys.sh
@@ -2,5 +2,5 @@ export YC_TOKEN=$(yc iam create-token)
 export YC_CLOUD_ID=$(yc config get cloud-id)
 export YC_FOLDER_ID=$(yc config get folder-id)
 export TF_VAR_folder_id=$YC_FOLDER_ID
-export AWS_ACCESS_KEY_ID=YCAJE6I4NA5ZHPXF0htWij2Qn
-export AWS_SECRET_ACCESS_KEY=YCPa8TeCPuBEampbDz4E2YiDi6hpklAhpdNLMTUp
+export AWS_ACCESS_KEY_ID=YCAJEKmZrk_TW35s1VJMP9m3b
+export AWS_SECRET_ACCESS_KEY=YCMapBCwxcZ3MXmE6X0j0AIw1RzsSzvZItNI0EwH
